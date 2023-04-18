385dd585810a1e0e978c372a0ecf9137b949aa96
vidaleishon <jo.vidalei@gmail.com>
2023-04-18 01:26:01 -0300

    apariencia

---
+++ apartamentoscordoba/src/Componentes/Menu.jsx
@@ -0,0 +1,33 @@
+function Menu() {
+
+    return (  
+          
+        <body>
+        <nav class="navbar navbar-expand-lg navbar-dark px-3">
+            <figure class="nav_logo">
+                <a href="./index.html"><img src="./imagenes/logo.png" alt="logo Apartamentos" width="200px" /></a>
+            </figure>
+            <div class="collapse navbar-collapse justify-content-end" id="navtoggler-aboutus">
+                <ul class="nav_menu">
+                    <li>
+                        <a>Departamentos</a>
+                    </li>
+                    <li>
+                        <a>Galeria</a>
+                    </li>
+                    <li>
+                        <a>Contacto</a>
+                    </li>
+                    <li>
+                        <a>Sobre nosotros</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        
+      </body>
+      )
+  
+  }
+
+  export default Menu
