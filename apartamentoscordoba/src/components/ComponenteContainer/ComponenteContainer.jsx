7718d5137a531eab825f19750ac9267ea2947044
Vachu <89685214+ValentinoGhitti@users.noreply.github.com>
2023-08-02 21:02:07 -0300

    Update ComponenteContainer.jsx

--- apartamentoscordoba/src/components/ComponenteContainer/ComponenteContainer.jsx
+++ apartamentoscordoba/src/components/ComponenteContainer/ComponenteContainer.jsx
@@ -1,60 +1,55 @@
-import { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react";
 import { gFetch } from "../../utils/gFetch";
 import { Link } from "react-router-dom";
 import ItemCount from "../ItemCount/ItemCounts";
 import Galerias from "../Galeria/Galerias";
-export const ComponenteContainer = ({ catalogo }) => {
 
-    const [departamentos, setDepartamentos] = useState([])
-    const [loading, setLoading] = useState(true)
+const ComponenteContainer = ({ catalogo }) => {
+  const [departamentos, setDepartamentos] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        gFetch()
-            .then(resp => setDepartamentos(resp))
-            .catch(err => console.log(err))
-            .then(resp => console.log(resp))
-            .finally(() => setLoading(false))
-    }, [])
+  useEffect(() => {
+    gFetch()
+      .then((resp) => {
+        setDepartamentos(resp);
+        setLoading(false);
+      })
+      .catch((err) => console.error(err));
+  }, []);
 
-    console.log(departamentos)
+  return (
+    <div>
+      <h1>Departamentos</h1>
+      {loading ? (
+        <div className="loading"></div>
+      ) : (
+        <div className="flexarticle">
+          {departamentos.map((departamento) => (
+            <article key={departamento.id} className="container grid">
+              <h2>Apartamento {departamento.name}</h2>
+              <div>
+                <img src={departamento.foto} className="grid_img" alt={departamento.name} />
+              </div>
+              <div className="descripcion">
+                <p className="grid_p">{departamento.descripcion}</p>
+              </div>
+              <div>
+                <h3>Precio por noche: {departamento.precio}</h3>
+              </div>
+              <div className="card-footer">
+                <Link to={`/galeria`}>
+                  <button>Galería</button>
+                </Link>
+                <div>
+                  <ItemCount />
+                </div>
+              </div>
+            </article>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
 
-    return (
-        <>
-            <h1>Departamentos</h1>
-            {loading ?
-                <div class="loading"></div>
-                :
-                <div class="flexarticle">
-
-                    {departamentos.map(departamento =>
-                        <article key={departamento.id} class="container grid">
-                            <h2>
-                                Apartamento {departamento.name}
-                            </h2>
-                            <div>
-                                <img src={departamento.foto} class="grid_img" />
-                            </div>
-                            <div class="descripcion">
-                                <p class="grid_p">{departamento.descripcion}</p>
-
-                            </div>
-                            <div>
-                                <h3>Precio por noche: {departamento.precio}</h3>
-                            </div>
-                            <div className="card-footer">
-                                <Link to={`/galeria`}>     
-                                    <button>Galería</button>      
-                                </Link>
-                                <div>
-                                    <ItemCount />
-                                </div>
-                            </div>
-
-                        </article>)}
-
-                </div >
-            }
-        </>
-    )
-
-}
+export default ComponenteContainer;
